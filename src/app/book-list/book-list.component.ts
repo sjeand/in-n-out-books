acff7f91a0ce50ef397708fdbe4952c829af9fa8
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-09-26 16:35:35 -0500

    first commit

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,73 @@
+/*
+==========================================
+  Title: in-n-out book
+  Author: Professor Krasso
+  Modifier: Sarah Jean Baptiste
+  Date: 09/08/2021
+  Description: part four
+==========================================
+*/
+
+//import statements
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
+import { IBook } from '../book.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { BookDetailsDialogComponent } from '../book-details-dialog/book-details-dialog.component';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+
+// export class
+export class BookListComponent implements OnInit {
+
+  books: Array<IBook> = [];
+  book: IBook | undefined;
+
+  constructor(private booksService: BooksService, private dialog: MatDialog) {
+    this.booksService.getBooks().subscribe((res:any) => {
+      console.log(res);
+      for (let key in res) {
+        if (res.hasOwnProperty(key)) {
+          let authors = [];
+          if (res[key].details.authors) {
+            authors = res[key].details.authors.map(function(author: { name: any; }) {
+              return author.name;
+            })
+          }
+          this.books.push({
+            isbn: res[key].details.isbn_13 ? res[key].details.isbn_13 : res[key].details.isbn_10,
+            title: res[key].details.title,
+            description: res[key].details.subtitle ? res[key].details.subtitle : 'N/A',
+            numOfPages: res[key].details.number_of_pages,
+            authors: authors
+          })
+        }
+      }
+    })
+  };
+
+  ngOnInit(): void { }
+
+  // function to show book details on dialog card
+  showBookDetails(isbn: string) {
+    this.book = this.books.find(book => book.isbn === isbn);
+
+    const dialogRef = this.dialog.open(BookDetailsDialogComponent, {
+      data: {
+        book: this.book
+      },
+      disableClose: true,
+      width: '800px'
+    })
+
+    console.log(this.book);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'confirm') { }
+    });
+  }
+}
